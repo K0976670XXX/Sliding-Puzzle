19533171abced93d4fbb61123b5f73b2100059b5
K0976670XXX <6213aab@gmail.com>
2026-04-11 05:00:37 +0800

    gif

--- image/out_json.py
+++ image/out_json.py
@@ -3,16 +3,27 @@ import json
 import sys, os 
 os.chdir(sys.path[0])#將當前環境位置設為當前"檔案位置"
 
+image_num = 55
+gif_num = 2
+
 data = {
     "images": []
 }
 
-for i in range(0, 55):
+for i in range(0, image_num):
     data["images"].append({
         "id": f"image-{i}",
         "name": f"Image {i}",
         "file": f"image ({i}).jpg",
         "alt": f"Puzzle image {i} preview"
+    })
+
+for i in range(0, gif_num):
+    data["images"].append({
+        "id": f"gif-{i}",
+        "name": f"gif {i}",
+        "file": f"gif ({i}).jpg",
+        "alt": f"Puzzle gif {i} preview"
     })
 
 with open("images.json", "w", encoding="utf-8") as f:
