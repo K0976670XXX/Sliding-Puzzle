e8e59b27c3d7ccf21b4213fff9dc176eacfb3c00
K0976670XXX <6213aab@gmail.com>
2026-04-11 03:41:39 +0800

    新增圖片

---
+++ image/out_json.py
@@ -0,0 +1,21 @@
+import json
+# 設定當前工作路徑
+import sys, os 
+os.chdir(sys.path[0])#將當前環境位置設為當前"檔案位置"
+
+data = {
+    "images": []
+}
+
+for i in range(0, 55):
+    data["images"].append({
+        "id": f"image-{i}",
+        "name": f"Image {i}",
+        "file": f"image ({i}).jpg",
+        "alt": f"Puzzle image {i} preview"
+    })
+
+with open("images.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
+print("images.json 已輸出完成")
